003f739ae8ac2c864297097ac46b2cbd4e57e018
Ganesh Vijayakumar <ganesh.vijayakumar@nrel.gov>
2020-05-31 08:04:44 -0600

    Adding a capability to compute fluctuating strain rate

--- src/derive/compute_strainrate.cpp
+++ src/derive/compute_strainrate.cpp
@@ -1,5 +1,7 @@
 #include "derive_K.H"
 #include "FieldRepo.H"
+#include "FieldPlaneAveraging.H"
+#include "DirectionSelector.H"
 
 namespace amr_wind {
 
@@ -143,7 +145,156 @@ void compute_strainrate(FType& field, const Field& vel)
     }
 }
 
+template<typename FType, typename IndexSelector>
+void compute_fluct_strainrate( FType& field,
+                               const Field& vel,
+                               const FieldPlaneAveraging& pa,
+                               const IndexSelector& pa_dir)
+{
+    const auto& repo = vel.repo();
+    const auto& geom_vec = repo.mesh().Geom();
+
+    const int nlevels = repo.num_active_levels();
+    for (int lev=0; lev < nlevels; ++lev) {
+        const auto& geom = geom_vec[lev];
+        const auto& domain = geom.Domain();
+
+        const amrex::Real dx = geom.CellSize()[0];
+        const amrex::Real dy = geom.CellSize()[1];
+        const amrex::Real dz = geom.CellSize()[2];
+
+        const amrex::Real idx = 1.0 / dx;
+        const amrex::Real idy = 1.0 / dy;
+        const amrex::Real idz = 1.0 / dz;
+
+        for (amrex::MFIter mfi(field(lev)); mfi.isValid(); ++mfi) {
+            const auto& bx = mfi.growntilebox(field.num_grow());
+            const auto& sr_arr = field(lev).array(mfi);
+            const auto& vel_arr = vel(lev).const_array(mfi);
+
+            amrex::ParallelFor(
+                bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                    sr_arr(i, j, k) = strainrate<StencilInterior>(
+                        i, j, k, idx, idy, idz, vel_arr, pa, pa_dir(i,j,k));
+                });
+
+            // TODO: Check if the following is correct for `foextrap` BC types
+            const auto& bxi = mfi.tilebox();
+            int idim = 0;
+            if (!geom.isPeriodic(idim)) {
+                if (bxi.smallEnd(idim) == domain.smallEnd(idim)) {
+                    amrex::IntVect low(bxi.smallEnd());
+                    amrex::IntVect hi(bxi.bigEnd());
+                    int sm = low[idim];
+                    low.setVal(idim, sm);
+                    hi.setVal(idim, sm);
+
+                    auto bxlo = amrex::Box(low, hi).grow({0, 1, 1});
+
+                    amrex::ParallelFor(
+                        bxlo, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                            sr_arr(i, j, k) = strainrate<StencilILO>(
+                                i, j, k, idx, idy, idz, vel_arr, pa, pa_dir(i,j,k));
+                        });
+                }
+
+                if (bxi.bigEnd(idim) == domain.bigEnd(idim)) {
+                    amrex::IntVect low(bxi.smallEnd());
+                    amrex::IntVect hi(bxi.bigEnd());
+                    int sm = hi[idim];
+                    low.setVal(idim, sm);
+                    hi.setVal(idim, sm);
+
+                    auto bxhi = amrex::Box(low, hi).grow({0, 1, 1});
+
+                    amrex::ParallelFor(
+                        bxhi, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                            sr_arr(i, j, k) = strainrate<StencilIHI>(
+                                i, j, k, idx, idy, idz, vel_arr, pa, pa_dir(i,j,k));
+                        });
+                }
+            } // if (!geom.isPeriodic)
+
+            idim = 1;
+            if (!geom.isPeriodic(idim)) {
+                if (bxi.smallEnd(idim) == domain.smallEnd(idim)) {
+                    amrex::IntVect low(bxi.smallEnd());
+                    amrex::IntVect hi(bxi.bigEnd());
+                    int sm = low[idim];
+                    low.setVal(idim, sm);
+                    hi.setVal(idim, sm);
+
+                    auto bxlo = amrex::Box(low, hi).grow({1, 0, 1});
+
+                    amrex::ParallelFor(
+                        bxlo, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                            sr_arr(i, j, k) = strainrate<StencilJLO>(
+                                i, j, k, idx, idy, idz, vel_arr, pa, pa_dir(i,j,k));
+                        });
+                }
+
+                if (bxi.bigEnd(idim) == domain.bigEnd(idim)) {
+                    amrex::IntVect low(bxi.smallEnd());
+                    amrex::IntVect hi(bxi.bigEnd());
+                    int sm = hi[idim];
+                    low.setVal(idim, sm);
+                    hi.setVal(idim, sm);
+
+                    auto bxhi = amrex::Box(low, hi).grow({1, 0, 1});
+
+                    amrex::ParallelFor(
+                        bxhi, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                            sr_arr(i, j, k) = strainrate<StencilJHI>(
+                                i, j, k, idx, idy, idz, vel_arr, pa, pa_dir(i,j,k));
+                        });
+                }
+            } // if (!geom.isPeriodic)
+
+            idim = 2;
+            if (!geom.isPeriodic(idim)) {
+                if (bxi.smallEnd(idim) == domain.smallEnd(idim)) {
+                    amrex::IntVect low(bxi.smallEnd());
+                    amrex::IntVect hi(bxi.bigEnd());
+                    int sm = low[idim];
+                    low.setVal(idim, sm);
+                    hi.setVal(idim, sm);
+
+                    auto bxlo = amrex::Box(low, hi).grow({1, 1, 0});
+
+                    amrex::ParallelFor(
+                        bxlo, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                            sr_arr(i, j, k) = strainrate<StencilKLO>(
+                                i, j, k, idx, idy, idz, vel_arr, pa, pa_dir(i,j,k));
+                        });
+                }
+
+                if (bxi.bigEnd(idim) == domain.bigEnd(idim)) {
+                    amrex::IntVect low(bxi.smallEnd());
+                    amrex::IntVect hi(bxi.bigEnd());
+                    int sm = hi[idim];
+                    low.setVal(idim, sm);
+                    hi.setVal(idim, sm);
+
+                    auto bxhi = amrex::Box(low, hi).grow({1, 1, 0});
+
+                    amrex::ParallelFor(
+                        bxhi, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                            sr_arr(i, j, k) = strainrate<StencilKHI>(
+                                i, j, k, idx, idy, idz, vel_arr, pa, pa_dir(i,j,k));
+                        });
+                }
+            } // if (!geom.isPeriodic)
+        }
+    }
+}
+
 template void compute_strainrate<Field>(Field&, const Field&);
 template void compute_strainrate<ScratchField>(ScratchField&, const Field&);
+template void compute_fluct_strainrate<Field,XDir>(Field&, const Field&, const FieldPlaneAveraging&, const XDir& );
+template void compute_fluct_strainrate<Field,YDir>(Field&, const Field&, const FieldPlaneAveraging&, const YDir& );
+template void compute_fluct_strainrate<Field,ZDir>(Field&, const Field&, const FieldPlaneAveraging&, const ZDir& );
+template void compute_fluct_strainrate<ScratchField,XDir>(ScratchField&, const Field&, const FieldPlaneAveraging&, const XDir&);
+template void compute_fluct_strainrate<ScratchField,YDir>(ScratchField&, const Field&, const FieldPlaneAveraging&, const YDir&);
+template void compute_fluct_strainrate<ScratchField,ZDir>(ScratchField&, const Field&, const FieldPlaneAveraging&, const ZDir&);
 
 }
