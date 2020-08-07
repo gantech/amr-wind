820b96664fbd131a5e1b66c7a1521158713016ab
gantech <ganesh.iitm@gmail.com>
2020-08-07 16:12:26 +0000

    Deploying to gh-pages from  @ f13273e0a9c844fdb5015794b3e1f760d34f43dd 🚀

---
+++ search/files_b.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['oneeqksgs_2ecpp',['OneEqKsgs.cpp',['../_one_eq_ksgs_8cpp.html',1,'']]],
+  ['oneeqksgs_2eh',['OneEqKsgs.H',['../_one_eq_ksgs_8_h.html',1,'']]],
+  ['oversetmanager_2eh',['OversetManager.H',['../_overset_manager_8_h.html',1,'']]]
+];
---
+++ search/functions_16.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['xlo',['xlo',['../classamr__wind_1_1_f_plane_averaging.html#a0733f57659b2e04cfbd9c033109cd3bc',1,'amr_wind::FPlaneAveraging']]],
+  ['xpay',['xpay',['../group__field__ops.html#ga010bc2107b9a3caf7e075d25d8c9e5fd',1,'amr_wind::field_ops::xpay(T1 &amp;dst, amrex::Real a, const T2 &amp;src, int srccomp, int dstcomp, int numcomp, const amrex::IntVect &amp;nghost)'],['../group__field__ops.html#gac0f6b51b9191825275331820bc620d8e',1,'amr_wind::field_ops::xpay(T1 &amp;dst, amrex::Real a, const T2 &amp;src, int srccomp, int dstcomp, int numcomp, int nghost)']]]
+];
